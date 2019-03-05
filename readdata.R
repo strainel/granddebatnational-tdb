abd265048122f43b8905d85cacfa2d1c7faf32ab
Stéphane Trainel <stephane.trainel@gmail.com>
2019-03-05 16:34:53 +0100

    update get data from graphql with cursor

--- readdata.R
+++ readdata.R
@@ -2,47 +2,70 @@ require(httr)
 require(jsonlite)
 require(dplyr)
 
-filecache <- "cache/listevt.Rdata"
+filecache <- "cache/listevt.Rdata" # fichier de données en cache
+
+if (file.exists(filecache)) {        # chargement des données en cache si elles existent
+  load(filecache)
+  } else {
+    listevt <- data.frame()
+    cursor <- 'null'
+  }
+
 
 # Définition des paramétres --------------------------------------------------
 
 url <- "https://granddebat.fr/graphql/internal"
 query <- "query EventRefetchRefetchQuery($cursor: String, $count: Int, $theme: ID, $project: ID, $userType: ID, $search: String, $isFuture: Boolean) { events(first: $count, after: $cursor, theme: $theme, project: $project, search: $search, userType: $userType, isFuture: $isFuture) { totalCount edges { node { id lat lng url fullAddress createdAt startAt endAt title fullAddress url themes { id title url } __typename } cursor } pageInfo { hasPreviousPage hasNextPage startCursor endCursor } } }"
-query_variables <- '{"cursor": null,"count": 5000,"theme": null,"project": null,"userType": null,"search": null,"isFuture": null}'
 
-pbody <- list(query = query, variables = query_variables)
-res <- POST(url, body = pbody, encode="json")
+count <- 100
+i <- 0
+nextPage <- TRUE
+liste <- data.frame()
+while (nextPage && i<20) {
+  query_variables <- paste0('{"cursor": "', cursor, '","count": ', count, ',"theme": null,"project": null,"userType": null,"search": null,"isFuture": null}')
+  i<-i+1
+  print(i)
+  print(query_variables)
+  pbody <- list(query = query, variables = query_variables)
+  res <- POST(url, body = pbody, encode="json")
+  
+  rparsed <- httr::content(res, as = "parsed", encoding = "UTF-8")
+  totalcount <- rparsed$data$events$totalCount
+  cursor <- rparsed$data$events$pageInfo$endCursor
+  nextPage <- rparsed$data$events$pageInfo$hasNextPage
 
-rparsed <- content(res, as = "parsed", encoding = "UTF-8")
-totalcount <- rparsed$data$events$totalCount
-rtext <- content(res, as = "text", encoding = "UTF-8")
-x <- fromJSON (rtext, flatten = TRUE)
-listevt <- bind_rows(x$data$events$edges)
-listevt <- filter(listevt, !is.na(cursor))
+  rtext <- httr::content(res, as = "text", encoding = "UTF-8")
+  x <- fromJSON (rtext, flatten = TRUE)
+  liste <- rbind(liste, bind_rows(x$data$events$edges))
+}
+liste <- filter(liste, !is.na(cursor))
+
 
 # Mise au format date
-listevt[,"node.createdAt"] <- as.POSIXct(listevt[,"node.createdAt"], format="%Y-%m-%d %H:%M:%S")
-listevt[,"node.startAt"] <- as.POSIXct(listevt[,"node.startAt"], format="%Y-%m-%d %H:%M:%S")
-listevt[,"node.endAt"] <- as.POSIXct(listevt[,"node.endAt"], format="%Y-%m-%d %H:%M:%S")
+liste[,"node.createdAt"] <- as.POSIXct(liste[,"node.createdAt"], format="%Y-%m-%d %H:%M:%S")
+liste[,"node.startAt"] <- as.POSIXct(liste[,"node.startAt"], format="%Y-%m-%d %H:%M:%S")
+liste[,"node.endAt"] <- as.POSIXct(liste[,"node.endAt"], format="%Y-%m-%d %H:%M:%S")
 
 # Ajout d'une variable createdAt pour avoir le jour de création de l'event
-listevt$createdAt <- as.character(round(listevt[,"node.createdAt"], "day"))
-listevt$createdAt <- as.Date(listevt$createdAt, "%Y-%m-%d")
+liste$createdAt <- as.character(round(liste[,"node.createdAt"], "day"))
+liste$createdAt <- as.Date(liste$createdAt, "%Y-%m-%d")
 
 # Ajout d'une variable startAt pour avoir le jour de création de l'event
-listevt$startAt <- as.character(round(listevt[,"node.startAt"], "day"))
-listevt$startAt <- as.Date(listevt$startAt, "%Y-%m-%d")
+liste$startAt <- as.character(round(liste[,"node.startAt"], "day"))
+liste$startAt <- as.Date(liste$startAt, "%Y-%m-%d")
 
 # Ajout d'une variable weekDay pour avoir le jour de création de l'event
-listevt$weekDay <- format(listevt$startAt, "%A")
-listevt$weekDay <- factor (listevt$weekDay, levels = c("lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"))
+liste$weekDay <- format(liste$startAt, "%A")
+liste$weekDay <- factor (liste$weekDay, levels = c("lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"))
 
 # Calcul du code postal et du département à partir de l'adresse
-r <- regexpr('[0-9]{5}',listevt$node.fullAddress)
+r <- regexpr('[0-9]{5}',liste$node.fullAddress)
 r2 <- r +5
-listevt$CP[r != -1] <- substr(listevt$node.fullAddress[r != -1], r[r != -1], r2[r != -1])
-listevt$DEP <- substr(listevt$CP, 0, 2)
-listevt$Commune[r != -1] <- toupper(substr(listevt$node.fullAddress[r != -1], r2[r != -1]+1, nchar(listevt$node.fullAddress[r != -1])))
+liste$CP[r != -1] <- substr(liste$node.fullAddress[r != -1], r[r != -1], r2[r != -1])
+liste$DEP <- substr(liste$CP, 0, 2)
+liste$Commune[r != -1] <- toupper(substr(liste$node.fullAddress[r != -1], r2[r != -1]+1, nchar(liste$node.fullAddress[r != -1])))
+
+listevt <- rbind(listevt,liste)
 
 # Read data commune & CP
 #require(data.table)
@@ -85,5 +108,5 @@ tmots <- sort(rowSums(mmots),decreasing=TRUE)
 dfmots <- data.frame(word = names(tmots),freq=tmots)
 
 # Sauvegarde dans un fichier "cache"
-save(listevt,totalcount, dfmots, file=filecache)
+save(listevt,cursor,totalcount, dfmots, file=filecache)
 
